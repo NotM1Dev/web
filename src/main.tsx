5145379f1c3bc4723c38f8da2664447fefcce280
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-01 12:45:31 +0200

    chore: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { ThemeProvider } from '@/components/themes/ThemeProvider.tsx';
 import { Toaster } from 'sonner';
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider defaultTheme="system" storageKey="m1-web-theme">
       <Toaster />
+      <SpeedInsights />
       <App />
     </ThemeProvider>
   </React.StrictMode>
