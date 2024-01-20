0d4a4b62f75db1b93ae5debc3c089e9b3724f409
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-20 13:01:20 +0200

    style: fix formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { AvatarImage, AvatarFallback, Avatar } from '@/components/ui/avatar';
-import { CardTitle, CardDescription, CardContent, Card } from '@/components/ui/card';
+import {
+    CardTitle,
+    CardDescription,
+    CardContent,
+    Card
+} from '@/components/ui/card';
 
+import { AvatarImage, AvatarFallback, Avatar } from '@/components/ui/avatar';
 import Links from '@/components/main/Links';
 
 export const runtime = 'edge';
@@ -10,39 +15,55 @@ const App = () => {
         <div
             key="1"
             className="
-        bg-white
-        text-black
-        dark:bg-black
-        dark:text-white
-        min-h-screen
-        px-6
-        py-8
-        flex
-        items-center
-        justify-center
-      "
+                bg-white
+                text-black
+                dark:bg-black
+                dark:text-white
+                min-h-screen
+                px-6
+                py-8
+                flex
+                items-center
+                justify-center
+            "
         >
             <Card
                 className="
-          flex
-          flex-col
-          md:flex-row
-          items-start
-          space-x-0
-          md:space-x-6
-          max-w-2xl
-          mx-auto
-          border-0
-          bg-transparent
-        "
+                    flex
+                    flex-col
+                    md:flex-row
+                    items-start
+                    space-x-0
+                    md:space-x-6
+                    max-w-2xl
+                    mx-auto
+                    border-0
+                    bg-transparent
+                "
             >
-                <Avatar className="h-36 w-36 border border-neutral-900 mb-4 md:mb-0">
+                <Avatar
+                    className="
+                        h-36
+                        w-36
+                        border
+                        border-neutral-900
+                        mb-4
+                        md:mb-0
+                    "
+                >
                     <AvatarImage src="https://github.com/m1-dev.png" />
                     <AvatarFallback>M1</AvatarFallback>
                 </Avatar>
                 <CardContent>
                     <CardTitle className="text-3xl font-bold">M1</CardTitle>
-                    <CardDescription className="text-gray-500 dark:text-gray-400 mt-2 text-lg">
+                    <CardDescription
+                        className="
+                            text-gray-500
+                            dark:text-gray-400
+                            mt-2
+                            text-lg
+                        "
+                    >
                         Self-taught developer.
                     </CardDescription>
                     <Links />
