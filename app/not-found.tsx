ff123568636486c6b26af29c3069384de0be34f0
m1-dev <140819896+m1-dev@users.noreply.github.com>
2024-01-23 21:35:42 +0200

    feat: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+const NotFound = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="text-center">
+                <h1 className="font-bold text-6xl">what?</h1>
+                <p>this page was not found.</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
